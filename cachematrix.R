1790f1090415f0081746df99a07dbebe0c178521
log-ons <log_ons@yahoo.com>
2015-07-26 12:23:26 -0700

    Modify mean --> solve function

--- cachematrix.R
+++ cachematrix.R
@@ -10,11 +10,10 @@ makeCacheMatrix <- function(x = matrix()) {
         m <<- NULL
     }
     get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    
+    list(set = set, get = get,setinv = setinv,getinv = getinv)
 }
 
 
