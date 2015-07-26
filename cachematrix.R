2f867689ae3b8dabd4aaa7132e1801f2ad44a5fb
log-ons <log_ons@yahoo.com>
2015-07-26 12:33:25 -0700

    Add indent for readability.

--- cachematrix.R
+++ cachematrix.R
@@ -7,11 +7,15 @@ makeCacheMatrix <- function(x = matrix()) {
     
     ## makeCacheMatrix - Takes matrix input and if matrix already exit
     ##then return cached inverse matrix else null.
+  
     m <- NULL
-    set <- function(y) {
+    set <- function(y) 
+    {
         x <<- y
         m <<- NULL
     }
+
+    
     get <- function() x
     setinv <- function(solve) m <<- solve
     getinv <- function() m
@@ -25,17 +29,21 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
     # Checks if makeCacheMatrix return value then returns cached value
     # Else calculate inv matrix and cach it and also return inv matrix.
     
     m <- x$getinv()
-    if(!is.null(m)) {
+    if(!is.null(m)) 
+    {
         message("getting cached data")
         return(m)
     }
+        
+  
     mat.data <- x$get()
     m <- solve(mat.data, ...)
+
     x$setinv(m)
     m
 }
